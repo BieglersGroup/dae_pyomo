3712404c50bfd6b932cf8523327f53c8660174f8
dthierry <dmolinat@andrew.cmu.edu>
2018-05-15 21:47:12 -0400

    solve model

--- capd_short/gams/cstr_oc_gams.py
+++ capd_short/gams/cstr_oc_gams.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 from pyomo.environ import *
 from pyomo.core.kernel.expr import exp
 from pyomo.core.kernel.numvalue import value
+from pyomo.opt import SolverFactory
 import sys
 __author__ = "David Thierry"  #: May 2018
 
@@ -23,18 +24,6 @@ m.ncp = ncp
 
 m.i = Set(initialize=range(1, nfe + 1))
 m.j = Set(initialize=range(1, ncp + 1))
-
-m.C = Var(m.i, m.j, initialize=0.0)
-m.T = Var(m.i, m.j, initialize=0.0)
-m.u = Var(m.i, m.j, initialize=0.0)
-m.tt = Var(m.i, m.j, initialize=0.0)
-
-m.cdot = Var(m.i, m.j, initialize=0.0)
-m.tdot = Var(m.i, m.j, initialize=0.0)
-m.c0 = Var(m.i, initialize=0.0)
-m.t0 = Var(m.i, initialize=0.0)
-m.tt0 = Var(m.i, initialize=0.0)
-m.phi = Var()
 
 a_init = {}
 # a_init[0, 0] = 0.0
@@ -62,6 +51,7 @@ alpha_init = {0: 1.95e-04, 1: 1e+06, 2: 2e+03, 3: 1e-03}
 m.cinit = Param(initialize=0.1367)
 m.tinit = Param(initialize=0.7293)
 m.uinit = Param(initialize=390.0)
+
 m.cdes = Param(initialize=0.0944)
 m.tdes = Param(initialize=0.7766)
 m.udes = Param(initialize=340)
@@ -89,83 +79,109 @@ m.slopeu = Param()
 m.ii = Param()
 m.jj = Param()
 
-
-
-
-
 m.a = Param(m.j, m.j, initialize=a_init)
 m.alpha = Param([0, 1, 2, 3], initialize=alpha_init)
 m.h = Param(m.i, initialize=1/value(m.nfe))
 
-m.cguess = Param(m.i, m.j)
-m.tguess = Param(m.i, m.j)
-m.ttguess = Param(m.i, m.j)
-m.uguess = Param(m.i, m.j)
+# m.cguess = Param(m.i, m.j)
+# m.tguess = Param(m.i, m.j)
+# m.ttguess = Param(m.i, m.j)
+# m.uguess = Param(m.i, m.j)
 
+point = 0
+slopec = (value(m.cdes) - value(m.cinit)) / (m.nfe * m.ncp)
+slopet = (value(m.tdes) - value(m.tinit)) / (m.nfe * m.ncp)
+slopeu = (value(m.udes) - value(m.uinit)) / (m.nfe * m.ncp)
 
-def _fecolc_rule(m, i, j):
+cguess = {}
+tguess = {}
+ttguess = {}
+uguess = {}
+
+for i in range(1, m.nfe + 1):
+    for j in range(1, m.ncp + 1):
+        point += 1
+        cguess[i, j] = slopec * point + value(m.cinit)
+        tguess[i, j] = slopet * point + value(m.tinit)
+        ttguess[i, j] = value(m.time) * point
+        uguess[i, j] = slopeu * point + value(m.uinit)
+
+#: One can pass the dictionary, as long as the keys are defined within the index set.
+m.C = Var(m.i, m.j, initialize=cguess)
+m.T = Var(m.i, m.j, initialize=tguess)
+m.u = Var(m.i, m.j, initialize=uguess)
+m.tt = Var(m.i, m.j, initialize=ttguess)
+
+m.cdot = Var(m.i, m.j, initialize=0.0)
+m.tdot = Var(m.i, m.j, initialize=0.0)
+m.c0 = Var(m.i, initialize=0.0)
+m.t0 = Var(m.i, initialize=0.0)
+m.tt0 = Var(m.i, initialize=0.0)
+m.phi = Var()
+
+def _fecolc_rule(mod, i, j):
     #: Note the ambiguity between m.j and j inside the function.
-    if i <= m.nfe:
-        return m.C[i, j] == m.c0[i] + m.time * m.h[i] * sum(m.a[k, j] * m.cdot[i, k] for k in m.j)
+    if i <= mod.nfe:
+        return mod.C[i, j] == mod.c0[i] + mod.time * mod.h[i] * sum(mod.a[k, j] * mod.cdot[i, k] for k in mod.j)
     else:
         return Constraint.Skip
 
 
-def _fecolt_rule(m, i, j):
-    if i <= m.nfe:
-        return m.T[i, j] == m.t0[i] + m.time * m.h[i] * sum(m.a[k, j] * m.tdot[i, k] for k in m.j)
+def _fecolt_rule(mod, i, j):
+    if i <= mod.nfe:
+        return mod.T[i, j] == mod.t0[i] + mod.time * mod.h[i] * sum(mod.a[k, j] * mod.tdot[i, k] for k in mod.j)
     else:
         return Constraint.Skip
 
 
-def _fecoltt_rule(m, i, j):
-    if i <= m.nfe:
-        return m.tt[i, j] == m.tt0[i] + m.time * m.h[i] * sum(m.a[k, j] for k in m.j)
+def _fecoltt_rule(mod, i, j):
+    if i <= mod.nfe:
+        return mod.tt[i, j] == mod.tt0[i] + mod.time * mod.h[i] * sum(mod.a[k, j] for k in mod.j)
     else:
         return Constraint.Skip
 
 
-def _conc_rule(m, i):
-    if 1 < i <= m.nfe:
-        return m.c0[i] == m.c0[i-1] + m.time * m.h[i-1] * sum(m.cdot[i-1, j] * m.a[j, 3] for j in m.j)
+def _conc_rule(mod, i):
+    if 1 < i <= mod.nfe:
+        return mod.c0[i] == mod.c0[i-1] + mod.time * mod.h[i-1] * sum(mod.cdot[i-1, j] * mod.a[j, 3] for j in mod.j)
     else:
         return Constraint.Skip
 
 
-def _cont_rule(m, i):
-    if 1 < i <= m.nfe:
-        return m.t0[i] == m.t0[i-1] + m.time * m.h[i-1] * sum(m.tdot[i-1, j] * m.a[j, 3] for j in m.j)
+def _cont_rule(mod, i):
+    if 1 < i <= mod.nfe:
+        return mod.t0[i] == mod.t0[i-1] + mod.time * mod.h[i-1] * sum(mod.tdot[i-1, j] * mod.a[j, 3] for j in mod.j)
     else:
         return Constraint.Skip
 
 
-def _contt_rule(m, i):
-    if 1 < i <= m.nfe:
-        return m.tt0[i] == m.tt0[i-1] + m.time * m.h[i-1] * sum(m.a[j, 3] for j in m.j)
+def _contt_rule(mod, i):
+    if 1 < i <= mod.nfe:
+        return mod.tt0[i] == mod.tt0[i-1] + mod.time * mod.h[i-1] * sum(mod.a[j, 3] for j in mod.j)
     else:
         return Constraint.Skip
 
 
-def _odec_rule(m, i, j):
-    if i <= m.nfe:
-        return m.cdot[i, j] == (1 - m.C[i, j])/m.theta - m.k10 * exp(-m.n/m.T[i, j]) * m.C[i, j]
+def _odec_rule(mod, i, j):
+    if i <= mod.nfe:
+        return mod.cdot[i, j] == (1 - mod.C[i, j])/mod.theta - mod.k10 * exp(-mod.n/mod.T[i, j]) * mod.C[i, j]
 
 
-def _odet_rule(m, i, j):
-    if i <= m.nfe:
-        return m.tdot[i, j] == \
-               (m.yf - m.T[i, j])/m.theta + m.k10 * exp(-m.n/m.T[i, j]) * m.C[i, j] - \
-               m.alpha[0] * m.u[i, j] * (m.T[i, j] - m.yc)
+def _odet_rule(mod, i, j):
+    if i <= mod.nfe:
+        return mod.tdot[i, j] == \
+               (mod.yf - mod.T[i, j])/mod.theta + mod.k10 * exp(-mod.n/mod.T[i, j]) * mod.C[i, j] - \
+               mod.alpha[0] * mod.u[i, j] * (mod.T[i, j] - mod.yc)
     else:
         return Constraint.Skip
 
 
-def _ic_rule(m):
-    return m.c0[0] == m.cinit
+def _ic_rule(mod):
+    return mod.c0[0] == mod.cinit
 
 
-def _it_rule(m):
-    return m.t0[0] - m.init
+def _it_rule(mod):
+    return mod.t0[0] - mod.init
 
 
 m.FeColC = Constraint(m.i, m.j, rule=_fecolc_rule)
@@ -179,4 +195,31 @@ m.ConTt = Constraint(m.i, rule=_contt_rule)
 m.OdeTt = Constraint(m.i, m.j, rule=_odec_rule)
 m.OdeC = Constraint(m.i, m.j, rule=_odet_rule)
 
+for var in m.C.itervalues():
+    var.setlb(0)
+    var.setub(1)
 
+for var in m.T.itervalues():
+    var.setlb(0.1)
+    var.setub(1)
+
+for var in m.c0.itervalues():
+    var.setlb(0)
+    var.setub(value(m.cinit))
+
+for var in m.t0.itervalues():
+    var.setlb(0.1)
+    var.setub(1)
+
+
+def objective_rule(mod):
+    return sum(mod.h[i] * mod.a[j, 3] * (mod.alpha[1] * (mod.cdes - mod.C[i, j]) ** 2 +
+                                         mod.alpha[2] * (mod.tdes - mod.T[i, j]) ** 2 +
+                                         mod.alpha[3] * (mod.udes - mod.u[i, j]) ** 2)
+               for i in m.i for j in m.j)
+
+m.fobj = Objective(sense=minimize, rule=objective_rule)
+
+
+ipopt = SolverFactory('ipopt')
+ipopt.solve(m, tee=True)
